5e9e8a8dade3c39778b36c9bc596a7fc4e8f666c
Kiba Fox <kiba.fox@foxypossibilities.com>
2019-10-15 22:23:09 -0400

    fetch if repo already exists locally
    Also refactored scanning the repos file and parsing into separate funcs.

--- internal/repos/repos.go
+++ internal/repos/repos.go
@@ -6,63 +6,106 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/user"
 	"strings"
 
 	"gopkg.in/src-d/go-git.v4"
 )
 
 func Sync(path string) error {
+	repos, err := scanRepos(path)
+	if err != nil {
+		return fmt.Errorf("sync: %w", err)
+	}
+
+	for _, r := range repos {
+		if _, err := os.Stat(r.path); err == nil {
+			repo, err := git.PlainOpen(r.path)
+			if err != nil {
+				log.Printf("sync: error opening repo: %s: %v", r.path, err)
+				continue
+			}
+
+			err = repo.Fetch(&git.FetchOptions{})
+			if err != nil && err != git.NoErrAlreadyUpToDate {
+				log.Printf("sync: error fetching: %s: %v", r.path, err)
+				continue
+			}
+		} else {
+			_, err = git.PlainClone(r.path, false, &git.CloneOptions{
+				URL: r.url,
+			})
+			if err != nil {
+				log.Printf("sync: error cloning: %s: %v", r.path, err)
+				continue
+			}
+		}
+	}
+
+	return nil
+}
+
+type repo struct {
+	path string
+	url  string
+}
+
+func scanRepos(path string) (repos []repo, err error) {
+	repos = make([]repo, 0, 9)
+
 	file, err := os.Open(path)
 	if err != nil {
-		return fmt.Errorf("failed to open repos file: %w", err)
+		return nil, fmt.Errorf("failed to open repos file: %w", err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		str := scanner.Text()
-		if strings.HasPrefix(str, "#") || len(str) == 0 {
-			// ignore comments and empty lines
+
+		r, err := parseLine(str)
+		if err != nil {
+			log.Printf("error parsing: %s: %v", str, err)
 			continue
 		}
 
-		split := strings.SplitN(str, " ", 2)
-		if len(split) < 2 {
-			return errors.New("repos file needs to formatted: PATH REMOTE")
+		if r == nil {
+			continue
 		}
 
-		path, remote := split[0], split[1]
-		if strings.HasPrefix(path, "~/") {
-			path = usrDir() + path[1:]
-		}
-
-		log.Println("Path:", path, "Remote:", remote)
-
-		_, err = git.PlainClone(path, false, &git.CloneOptions{
-			URL: remote,
-		})
-		if err != nil {
-			return fmt.Errorf("failed to clone: %w", err)
-		}
+		repos = append(repos, *r)
 	}
 
 	if err := scanner.Err(); err != nil {
-		return fmt.Errorf("issue scanning repos file: %w", err)
+		return nil, fmt.Errorf("issue scanning repos file: %w", err)
 	}
 
-	return nil
+	return repos, nil
 }
 
-var home string
-
-func usrDir() string {
-	if home == "" {
-		usr, err := user.Current()
-		if err != nil {
-			log.Fatal(err)
-		}
-		home = usr.HomeDir
+func parseLine(line string) (r *repo, err error) {
+	if strings.HasPrefix(line, "#") || len(line) == 0 {
+		// ignore comments and empty lines
+		return nil, nil
 	}
-	return home
+
+	split := strings.SplitN(line, " ", 2)
+	if len(split) < 2 {
+		return nil, errors.New("repos file needs to formatted: PATH REMOTE")
+	}
+
+	r = &repo{
+		path: split[0],
+		url:  split[1],
+	}
+
+	if strings.HasPrefix(r.path, "~/") {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return nil, fmt.Errorf("error getting user home directory: %w", err)
+		}
+
+		r.path = home + r.path[1:]
+	}
+
+	return r, nil
 }
