a3acc6f68a6002360fd6c6687056a8a0abc20a31
Kiba Fox <kiba.fox@foxypossibilities.com>
2019-10-20 08:34:59 -0400

    Add more docs, list cmd, rename sync to fetch

--- cmd/repos/main.go
+++ cmd/repos/main.go
@@ -3,36 +3,81 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/kyoh86/xdg"
 	"github.com/urfave/cli"
 	"gitlab.com/KibaFox/repos/internal/repos"
 )
+
+const Version = "0.0.0"
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "repos"
 	app.Usage = "manage multiple git repositories"
+	app.Version = Version
+	app.Authors = []cli.Author{{Name: "Kiba Fox"}}
+	app.Description = strings.TrimSpace(`
+
+   repos uses configurations that define a list of repositories to in order to
+   help you manage them.
+
+   Configuration files can either be hand crafted or imported with the "import"
+   subcommand.  Configuration file names must end in ".repo" to be used.
+
+	`)
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "config, c",
+			Usage: "configuration directory path",
+			Value: filepath.Join(xdg.ConfigHome(), "repos"),
+		},
+	}
 
 	app.Commands = []cli.Command{
 		{
-			Name:    "sync",
+			Name:    "list",
 			Aliases: []string{},
-			Usage:   "clone or fetch git repos",
+			Usage:   "list configurations in the config directory",
 			Action: func(c *cli.Context) error {
-				return sync(c.Args().First())
+				return List(c.GlobalString("config"))
+			},
+		},
+		{
+			Name:    "fetch",
+			Aliases: []string{},
+			Usage:   "fetch repos from a config",
+			Action: func(c *cli.Context) error {
+				return Fetch(c.GlobalString("config"), c.Args().First())
 			},
 		},
 		{
 			Name:    "import",
 			Aliases: []string{},
-			Usage:   "walk paths with existing repos to create a configuration",
+			Usage:   "searches paths for existing repos to create a config",
+			Description: strings.TrimSpace(`
+   import will take each argument as a directory path and searches them for git
+   repositories to produce one configuration file that can be used with the
+   other subcommands.
+
+   Repositories are detected when a path contains a directory named ".git".  The
+   parent directory is then opened as a git repository to read the git config.
+   The first URL for the remote named "origin" is used for the import.
+			`),
 			Flags: []cli.Flag{
-				cli.StringFlag{Name: "output, o"},
+				cli.StringFlag{
+					Name:  "output, o",
+					Usage: "destination file for the import (default: stdout)",
+				},
 			},
 			Action: func(c *cli.Context) error {
-				return imp(c.String("output"), c.Args()...)
+				return Import(c.String("output"), c.Args()...)
 			},
 		},
 	}
@@ -43,7 +88,28 @@ func main() {
 	}
 }
 
-func sync(path string) error {
+func List(cfg string) error {
+	path := repos.ExpandHome(cfg)
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return fmt.Errorf("list: could not read config dir: %w", err)
+	}
+
+	for _, file := range files {
+		if !strings.HasPrefix(file.Name(), ".") &&
+			!file.IsDir() &&
+			strings.HasSuffix(file.Name(), ".repo") {
+			fmt.Println(file.Name()[0 : len(file.Name())-5])
+		}
+	}
+
+	return nil
+}
+
+func Fetch(cfg, name string) error {
+	path := filepath.Join(repos.ExpandHome(cfg), name+".repo")
+
 	file, err := os.Open(path)
 	if err != nil {
 		return fmt.Errorf("sync: failed to open repos file: %w", err)
@@ -63,7 +129,7 @@ func sync(path string) error {
 	return nil
 }
 
-func imp(out string, paths ...string) (err error) {
+func Import(out string, paths ...string) (err error) {
 	var output io.Writer
 
 	if out == "" {
